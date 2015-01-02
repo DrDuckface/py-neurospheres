688bc9e170fc3275b5542e1a6c4927a0a420ee78
briannaG <brianna.goldenstein@gmail.com>
2015-01-01 21:52:07 -0600

    [FEATURE] improves CLI argument capture

--- py-neurospheres.py
+++ py-neurospheres.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
-# Libraries
+# libraries
 import sys, getopt
 
-# Globals
+# globals
 folder = None
 min_d = None
 px_per_micron = None
 
 def main(argv):
-   	global folder, min_d, px_per_micron
-   	try:
+	global folder, min_d, px_per_micron
+	try: 
 		opts, args = getopt.getopt(argv, 'hi:d:s:', ['folder=', 'min_d=', 'px_per_micron='])
-   	except getopt.GetoptError:
-   		print 'Invalid input!'
-		print 'usage: py-neurospheres.py -i <folder> -d <min_d> -s <px_per_micron>'
+	except getopt.GetoptError:
+		print 'Invalid input'
+		print 'Usage: py-neurospheres.py -i <folder> -d <min_d> -s <px_per_micron>'
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt in ('-i', '--folder'):
@@ -23,14 +23,15 @@ def main(argv):
 			min_d = arg
 		elif opt in ('-s', '--px_per_micron'):
 			px_per_micron = arg
+	
 	if folder == None or min_d == None or px_per_micron == None:
-		print 'Invalid input!'
-		print 'usage: py-neurospheres.py -i <folder> -d <min_d> -s <px_per_micron>'
+		print 'Invalid input'
+		print 'Usage: py-neurospheres.py -i <folder> -d <min_d> -s <px_per_micron>'
 		sys.exit()
 	else:
-		print 'Input folder: ', folder
-		print 'Minimum diameter: ', min_d
-		print 'Image scale (px to microns): ', px_per_micron
-
-if __name__ == '__main__':
+		print 'input folder:', folder
+		print 'minimum diameter:', min_d
+		print 'image scale (pixels per micron):', px_per_micron
+		
+if __name__ == '__main__': 
 	main(sys.argv[1:])
