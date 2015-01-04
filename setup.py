b776a4d43d53afa6d3864fd56b07381c4e520946
briannaG <brianna.goldenstein@gmail.com>
2015-01-04 14:56:52 -0500

    [CHORE] project dependencies setup
    - registered with PyPI
    - established dependences using pip
    - dependency is skimage

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py-neurospheres',
+      version='1.0',
+      description='CLI analysis of neurosphere images',
+      author='Brianna Goldenstein',
+      author_email='brianna.goldenstein@gmail.com',
+      url='https://github.com/DrDuckface/py-neurospheres',
+      packages=[],
+     )
